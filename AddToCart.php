49d03e830bd8d954e38bc13d920262d1a8684e1e
MiguelOrino <31387037+MiguelOrino@users.noreply.github.com>
2018-08-02 14:33:30 +0800

    Add files via upload

---
+++ AddToCart.php
@@ -0,0 +1,31 @@
+<?php
+	if (session_status() == PHP_SESSION_NONE) 
+	{
+		session_start();
+	}
+	if(!$_SESSION["logged_in"])
+	{
+		include 'Dummy.php';
+	}
+	else
+	{
+		$conn = mysqli_connect("localhost", "root", "");
+		if(!$conn) {
+			die("Could not connect: ".mysqli_connect_error());
+		}
+		
+		$db_selected = mysqli_select_db($conn, "shopping");
+		if(!$db_selected) {
+			echo "Connection to the database failed. <br>";
+		}
+		
+		$userID = $_SESSION["UserID"];
+		$addID = $_POST["addID"];
+		$stmt = $conn->prepare("INSERT INTO cart (UserID, ItemID) VALUES (?, ?)");
+		$stmt->bind_param("ii", $userID, $addID);
+		$stmt->execute();
+		
+		echo '<script language="javascript">alert("Item has been added to your cart! Click on your cart to review and checkout");</script>';
+		include 'Catalog.php';
+	}
+?>
---
+++ DisplayCatalogItems.php
@@ -0,0 +1,52 @@
+<?php
+	$conn = mysqli_connect("localhost", "root", "");
+	if(!$conn) {
+		die("Could not connect: ".mysqli_connect_error());
+	}
+	
+	$db_selected = mysqli_select_db($conn, "shopping");
+	if(!$db_selected) {
+		echo "Connection to the database failed. <br>";
+	}
+	
+	$UserID = $_SESSION["UserID"];
+	$sql = "SELECT * FROM items";
+	$result = mysqli_query($conn, $sql);
+	$items = 0;
+	if($result->num_rows > 0)
+	{
+		while ($row = mysqli_fetch_array($result))
+		{
+			$itemID = $row["ItemID"];
+			$itemName = $row["Item_Name"];
+			$itemDes = $row["Item_Description"];
+			if($items == 0)
+			{
+				echo '<div class="row">';
+			}
+			echo '<div class="col">
+						<div class="card" style="width: 18rem;">
+							<img class="card-img-top" src="images/'.$itemID.'.jpg" alt="Card image cap">
+								<div class="card-body">
+								<h5 class="card-title">'.$itemName.'</h5>
+								<p class="card-text">'.$itemDes.'</p>
+								<form method="post" action="AddToCart.php">
+									<input type="submit" class="btn btn-warning btn-block"  value="Add to Cart"></button>	<!-- delete function-->
+									<input type="hidden" name="addID" value="'.$itemID.'">
+								</form>
+							</div>
+						</div>
+					</div>';
+			$items++;
+			if($items == 3)
+			{
+				echo '</div>
+					<br>';
+			}
+		}
+	}
+	else
+	{
+		echo '<h2>Sorry, we seem to be out of stock for everything! Check back next time for new incoming batches!<h2>';
+	}
+?>
